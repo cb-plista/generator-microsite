18b3d8c4dd4c2dc1f1f5c5f39ce2761ffe8b1a1d
Christian Bieber <cb@plista.com>
2013-12-11 11:32:24 +0100

    no message

--- app/index.js
+++ app/index.js
@@ -1,73 +1,315 @@
 'use strict';
-var util = require('util');
 var path = require('path');
+var util = require('util');
+var angularUtils = require('../util.js');
+var spawn = require('child_process').spawn;
 var yeoman = require('yeoman-generator');
 
 
-/**
- * This is our constructor function of the generator
- * @type {Function}
- */
-var Generator = module.exports = function Generator(args, options, config) {
-  /*
-   With these two calls, we are inheriting functionality from both Base and NamedBase.
-   That's how we were able to call that this.prompt function, as well as many others.
-   */
+var Generator = module.exports = function Generator(args, options) {
   yeoman.generators.Base.apply(this, arguments);
+  this.argument('appname', { type: String, required: false });
+  this.appname = this.appname || path.basename(process.cwd());
+  this.appname = this._.camelize(this._.slugify(this._.humanize(this.appname)));
 
-  // creates a listener for the end event (when the methods on the prototype finish executing)
-  this.on('end', function () {
-    this.installDependencies({ skipInstall: options['skip-install'] });
+  this.option('app-suffix', {
+    desc: 'Allow a custom suffix to be added to the module name',
+    type: String,
+    required: 'false'
+  });
+  this.scriptAppName = this.appname + angularUtils.appName(this);
+
+  args = ['main'];
+
+  if (typeof this.env.options.appPath === 'undefined') {
+    try {
+      this.env.options.appPath = require(path.join(process.cwd(), 'bower.json')).appPath;
+    } catch (e) {}
+    this.env.options.appPath = this.env.options.appPath || 'app';
+  }
+
+  this.appPath = this.env.options.appPath;
+
+  if (typeof this.env.options.coffee === 'undefined') {
+    this.option('coffee', {
+      desc: 'Generate CoffeeScript instead of JavaScript'
+    });
+
+    // attempt to detect if user is using CS or not
+    // if cml arg provided, use that; else look for the existence of cs
+    if (!this.options.coffee &&
+      this.expandFiles(path.join(this.appPath, '/scripts/**/*.coffee'), {}).length > 0) {
+      this.options.coffee = true;
+    }
+
+    this.env.options.coffee = this.options.coffee;
+  }
+
+  if (typeof this.env.options.minsafe === 'undefined') {
+    this.option('minsafe', {
+      desc: 'Generate AngularJS minification safe code'
+    });
+    this.env.options.minsafe = this.options.minsafe;
+    args.push('--minsafe');
+  }
+
+  this.hookFor('angular:common', {
+    args: args
   });
 
-  // gives us access to our generator's package.json file
+  this.hookFor('angular:main', {
+    args: args
+  });
+
+  this.hookFor('angular:controller', {
+    args: args
+  });
+
+  this.on('end', function () {
+    this.installDependencies({ skipInstall: this.options['skip-install'] });
+
+    var enabledComponents = [];
+
+    if (this.resourceModule) {
+      enabledComponents.push('angular-resource/angular-resource.js');
+    }
+
+    if (this.cookiesModule) {
+      enabledComponents.push('angular-cookies/angular-cookies.js');
+    }
+
+    if (this.sanitizeModule) {
+      enabledComponents.push('angular-sanitize/angular-sanitize.js');
+    }
+
+    if (this.routeModule) {
+      enabledComponents.push('angular-route/angular-route.js');
+    }
+
+    this.invoke('karma:app', {
+      options: {
+        coffee: this.options.coffee,
+        travis: true,
+        'skip-install': this.options['skip-install'],
+        components: [
+          'angular/angular.js',
+          'angular-mocks/angular-mocks.js'
+        ].concat(enabledComponents)
+      }
+    });
+  });
+
   this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
 };
 
 util.inherits(Generator, yeoman.generators.Base);
 
-Generator.prototype.askFor = function askFor() {
+Generator.prototype.welcome = function welcome() {
+  // welcome message
+  if (!this.options['skip-welcome-message']) {
+    console.log(this.yeoman);
+    console.log(
+      'Out of the box I include Bootstrap and some AngularJS recommended modules.\n'
+    );
+
+    // Deprecation notice for minsafe
+    if (this.options.minsafe) {
+      console.warn(
+        '\n** The --minsafe flag is being deprecated in 0.7.0 and removed in ' +
+        '0.8.0. For more information, see ' +
+        'https://github.com/yeoman/generator-angular#minification-safe. **\n'
+      );
+    }
+  }
+};
+
+Generator.prototype.askForBootstrap = function askForBootstrap() {
   var cb = this.async();
 
-  // have Yeoman greet the user.
-  console.log('-------------------------------------------------');
-  console.log('-- plista microsite - GENERATOR');
-  console.log('-- author: cb@plista');
-  console.log('-- ( if u have any question don\'t hesitate )');
-  console.log('-------------------------------------------------');
+  this.prompt([{
+    type: 'confirm',
+    name: 'bootstrap',
+    message: 'Would you like to include Twitter Bootstrap?',
+    default: true
+  }, {
+    type: 'confirm',
+    name: 'compassBootstrap',
+    message: 'Would you like to use the SCSS version of Twitter Bootstrap with the Compass CSS Authoring Framework?',
+    default: true,
+    when: function (props) {
+      return props.bootstrap;
+    }
+  }], function (props) {
+    this.bootstrap = props.bootstrap;
+    this.compassBootstrap = props.compassBootstrap;
 
-  var prompts = [{
-    name: 'clientName',
-    message: 'what is the client name'
-  },{
-    name: 'campaignName',
-    message: 'what is the campaign name'
-  },{
-    name: 'campaignId',
-    message: 'what is the campaign id',
-    default: 0
-  }];
-
-  this.prompt(prompts, function (props) {
-    this.clientName = props.clientName || 'plista';
-    this.campaignName = props.campaignName || 'microsite';
-    this.campaignName = parseInt(props.campaignId, 10);
     cb();
   }.bind(this));
 };
 
-Generator.prototype.app = function app() {
-  this.mkdir('app');
-  this.mkdir('app/styles');
-  this.mkdir('app/images');
-  this.mkdir('app/js');
+Generator.prototype.askForModules = function askForModules() {
+  var cb = this.async();
 
-  this.copy('../../templates/_package.json', 'package.json');
-  this.copy('../../templates/_bower.json', 'bower.json');
-  this.copy('../../templates/_Gruntfile.js', 'Gruntfile.js');
+  var prompts = [{
+    type: 'checkbox',
+    name: 'modules',
+    message: 'Which modules would you like to include?',
+    choices: [{
+      value: 'resourceModule',
+      name: 'angular-resource.js',
+      checked: true
+    }, {
+      value: 'cookiesModule',
+      name: 'angular-cookies.js',
+      checked: true
+    }, {
+      value: 'sanitizeModule',
+      name: 'angular-sanitize.js',
+      checked: true
+    }, {
+      value: 'routeModule',
+      name: 'angular-route.js',
+      checked: true
+    }]
+  }];
+
+  this.prompt(prompts, function (props) {
+    var hasMod = function (mod) { return props.modules.indexOf(mod) !== -1; };
+    this.resourceModule = hasMod('resourceModule');
+    this.cookiesModule = hasMod('cookiesModule');
+    this.sanitizeModule = hasMod('sanitizeModule');
+    this.routeModule = hasMod('routeModule');
+
+    var angMods = [];
+
+    if (this.cookiesModule) {
+      angMods.push("'ngCookies'");
+    }
+
+    if (this.resourceModule) {
+      angMods.push("'ngResource'");
+    }
+    if (this.sanitizeModule) {
+      angMods.push("'ngSanitize'");
+    }
+    if (this.routeModule) {
+      angMods.push("'ngRoute'");
+      this.env.options.ngRoute = true;
+    }
+
+    if (angMods.length) {
+      this.env.options.angularDeps = "\n  " + angMods.join(",\n  ") +"\n";
+    }
+
+    cb();
+  }.bind(this));
 };
 
-Generator.prototype.projectfiles = function projectfiles() {
-  this.copy('../../templates/editorconfig', '.editorconfig');
-  this.copy('../../templates/jshintrc', '.jshintrc');
+Generator.prototype.readIndex = function readIndex() {
+  this.ngRoute = this.env.options.ngRoute;
+  this.indexFile = this.engine(this.read('../../templates/common/index.html'), this);
 };
+
+// Waiting a more flexible solution for #138
+Generator.prototype.bootstrapFiles = function bootstrapFiles() {
+  var sass = this.compassBootstrap;
+  var files = [];
+  var source = 'styles/' + ( sass ? 's' : '' ) + 'css/';
+
+  if (this.bootstrap && !sass) {
+    files.push('bootstrap.css');
+    this.copy('fonts/glyphicons-halflings-regular.eot', 'app/fonts/glyphicons-halflings-regular.eot');
+    this.copy('fonts/glyphicons-halflings-regular.ttf', 'app/fonts/glyphicons-halflings-regular.ttf');
+    this.copy('fonts/glyphicons-halflings-regular.svg', 'app/fonts/glyphicons-halflings-regular.svg');
+    this.copy('fonts/glyphicons-halflings-regular.woff', 'app/fonts/glyphicons-halflings-regular.woff');
+  }
+
+  files.push('main.' + (sass ? 's' : '') + 'css');
+
+  files.forEach(function (file) {
+    this.copy(source + file, 'app/styles/' + file);
+  }.bind(this));
+
+  this.indexFile = this.appendFiles({
+    html: this.indexFile,
+    fileType: 'css',
+    optimizedPath: 'styles/main.css',
+    sourceFileList: files.map(function (file) {
+      return 'styles/' + file.replace('.scss', '.css');
+    }),
+    searchPath: ['.tmp', 'app']
+  });
+};
+
+Generator.prototype.bootstrapJS = function bootstrapJS() {
+  if (!this.bootstrap) {
+    return;  // Skip if disabled.
+  }
+
+  // Wire Twitter Bootstrap plugins
+  this.indexFile = this.appendScripts(this.indexFile, 'scripts/plugins.js', [
+    'bower_components/sass-bootstrap/js/affix.js',
+    'bower_components/sass-bootstrap/js/alert.js',
+    'bower_components/sass-bootstrap/js/button.js',
+    'bower_components/sass-bootstrap/js/carousel.js',
+    'bower_components/sass-bootstrap/js/transition.js',
+    'bower_components/sass-bootstrap/js/collapse.js',
+    'bower_components/sass-bootstrap/js/dropdown.js',
+    'bower_components/sass-bootstrap/js/modal.js',
+    'bower_components/sass-bootstrap/js/scrollspy.js',
+    'bower_components/sass-bootstrap/js/tab.js',
+    'bower_components/sass-bootstrap/js/tooltip.js',
+    'bower_components/sass-bootstrap/js/popover.js'
+  ]);
+};
+
+Generator.prototype.extraModules = function extraModules() {
+  var modules = [];
+  if (this.resourceModule) {
+    modules.push('bower_components/angular-resource/angular-resource.js');
+  }
+
+  if (this.cookiesModule) {
+    modules.push('bower_components/angular-cookies/angular-cookies.js');
+  }
+
+  if (this.sanitizeModule) {
+    modules.push('bower_components/angular-sanitize/angular-sanitize.js');
+  }
+
+  if (this.routeModule) {
+    modules.push('bower_components/angular-route/angular-route.js');
+  }
+
+  if (modules.length) {
+    this.indexFile = this.appendScripts(this.indexFile, 'scripts/modules.js',
+        modules);
+  }
+};
+
+Generator.prototype.appJs = function appJs() {
+  this.indexFile = this.appendFiles({
+    html: this.indexFile,
+    fileType: 'js',
+    optimizedPath: 'scripts/scripts.js',
+    sourceFileList: ['scripts/app.js', 'scripts/controllers/main.js'],
+    searchPath: ['.tmp', 'app']
+  });
+};
+
+Generator.prototype.createIndexHtml = function createIndexHtml() {
+  this.indexFile = this.indexFile.replace(/&apos;/g, "'");
+  this.write(path.join(this.appPath, 'index.html'), this.indexFile);
+};
+
+Generator.prototype.packageFiles = function () {
+  this.coffee = this.env.options.coffee;
+  this.template('../../templates/common/_bower.json', 'bower.json');
+  this.template('../../templates/common/_package.json', 'package.json');
+  this.template('../../templates/common/Gruntfile.js', 'Gruntfile.js');
+};
+
+Generator.prototype.imageFiles = function () {
+  this.sourceRoot(path.join(__dirname, 'templates'));
+  this.directory('images', 'app/images', true);
+}
